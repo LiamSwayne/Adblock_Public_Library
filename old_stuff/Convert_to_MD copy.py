ebfb6b848af171d49a226ee42a62ca1ac86eff75
Liam Swayne <108629034+LiamSwayne@users.noreply.github.com>
2023-06-09 15:02:46 -0400

    Add files via upload

---
+++ old_stuff/Convert_to_MD copy.py
@@ -0,0 +1,21 @@
+# Take the content from selecting all and copying "My Filter List" and format it for markdown.
+# Content is pasted into "elementsStr".
+def processInput(elementsStr, MD=True):
+    lines = elementsStr.split("\n")
+    output = ""
+    
+    for i in range(len(lines)):
+        line = lines[i]
+        if line != "" and line != "\n" and line[0:10] != "data:image" and line != "youtube.com##.style-scope.ytd-rich-shelf-renderer":
+            if MD:
+                output += "  " + str(i) + ". " + line+"\n"
+            else:
+                output += line+"\n"
+    output = output[:-1]
+    print(output)
+
+elementsStr = '''
+# elements go here
+'''
+
+processInput(elementsStr)
